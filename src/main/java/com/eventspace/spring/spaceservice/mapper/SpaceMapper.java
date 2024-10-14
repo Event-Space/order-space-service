c485ae75627b9a80b7410671668bdd4258b16ebc
Karakat Almasova <karakatalmasova2@gmail.com>
2024-10-15 04:09:11 +0500

    deleted mappers

--- src/main/java/com/eventspace/spring/spaceservice/mapper/SpaceMapper.java
+++ src/main/java/com/eventspace/spring/spaceservice/mapper/SpaceMapper.java
@@ -3,24 +3,76 @@ package com.eventspace.spring.spaceservice.mapper;
 import com.eventspace.spring.spaceservice.dto.SpaceRequest;
 import com.eventspace.spring.spaceservice.dto.SpaceResponse;
 import com.eventspace.spring.spaceservice.model.entity.Space;
-import org.mapstruct.BeanMapping;
-import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
+@Component
+public class SpaceMapper {
 
-@Mapper(componentModel = "spring")
-public interface SpaceMapper {
-    SpaceMapper INSTANCE = Mappers.getMapper(SpaceMapper.class);
-    SpaceResponse spaceToSpaceResponse(Space space);
-    SpaceRequest spaceToSpaceRequest(Space space);
-    Space spaceRequestToSpace(SpaceRequest spaceRequest);
-    Space spaceResponseToSpace(SpaceResponse spaceResponse);
-    List<SpaceResponse> spaceListToSpaceResponseList(List<Space> spaces);
-    List<SpaceRequest> spaceListToSpaceRequestList(List<Space> spaces);
-    List<Space> spaceRequestListToSpaceList(List<SpaceRequest> spaceRequests);
-    List<Space> spaceResponseListToSpaceList(List<SpaceResponse> spaceResponses);
-//lol
+    // Convert Space to SpaceResponse
+    public SpaceResponse spaceToSpaceResponse(Space space) {
+        if (space == null) {
+            return null;
+        }
+
+        SpaceResponse response = new SpaceResponse();
+        response.setId(space.getId());
+        response.setAddress(space.getAddress());
+        response.setLocation(space.getLocation());
+        response.setSize(space.getSize());
+        response.setMaxCapacity(space.getMaxCapacity());
+        response.setBaseRentalCost(space.getBaseRentalCost());
+        response.setAvailabilityStatus(space.getAvailabilityStatus());
+        return response;
+    }
+
+    // Convert Space to SpaceRequest
+    public SpaceRequest spaceToSpaceRequest(Space space) {
+        if (space == null) {
+            return null;
+        }
+
+        SpaceRequest request = new SpaceRequest();
+        request.setName(space.getName());
+        request.setAddress(space.getAddress());
+        request.setLocation(space.getLocation());
+        request.setSize(space.getSize());
+        request.setMaxCapacity(space.getMaxCapacity());
+        request.setBaseRentalCost(space.getBaseRentalCost());
+        request.setAvailabilityStatus(space.getAvailabilityStatus());
+        return request;
+    }
+
+    // Convert SpaceRequest to Space
+    public Space spaceRequestToSpace(SpaceRequest spaceRequest) {
+        if (spaceRequest == null) {
+            return null;
+        }
+
+        Space space = new Space();
+        space.setName(spaceRequest.getName());
+        space.setAddress(spaceRequest.getAddress());
+        space.setLocation(spaceRequest.getLocation());
+        space.setSize(spaceRequest.getSize());
+        space.setMaxCapacity(spaceRequest.getMaxCapacity());
+        space.setBaseRentalCost(spaceRequest.getBaseRentalCost());
+        space.setAvailabilityStatus(spaceRequest.getAvailabilityStatus());
+        return space;
+    }
+
+    // Convert SpaceResponse to Space
+    public Space spaceResponseToSpace(SpaceResponse spaceResponse) {
+        if (spaceResponse == null) {
+            return null;
+        }
+
+        Space space = new Space();
+        space.setId(spaceResponse.getId()); // Assuming you want to set the ID in the entity
+        space.setAddress(spaceResponse.getAddress());
+        space.setLocation(spaceResponse.getLocation());
+        space.setSize(spaceResponse.getSize());
+        space.setMaxCapacity(spaceResponse.getMaxCapacity());
+        space.setBaseRentalCost(spaceResponse.getBaseRentalCost());
+        space.setAvailabilityStatus(spaceResponse.getAvailabilityStatus());
+        return space;
+    }
 }
