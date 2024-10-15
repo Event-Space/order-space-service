bf0fabe2cd5277ca894e03150e37ad3652c35433
Karakat Almasova <karakatalmasova2@gmail.com>
2024-10-15 19:38:32 +0500

    added swagger

---
+++ src/main/java/com/eventspace/spring/spaceservice/Configuration/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package com.eventspace.spring.spaceservice.Configuration;
+
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Event Space")
+                        .version("v1")
+                        .description("API Documentation for your project"));
+    }
+}
+
