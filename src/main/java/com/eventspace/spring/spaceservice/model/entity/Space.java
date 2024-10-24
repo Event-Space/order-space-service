fb50d5a980dd6e267ecde2622734841962bea666
rus1k <wd@>
2024-10-24 17:19:41 +0500

    feat(pre ci 2): Another pre ci things...

--- src/main/java/com/eventspace/spring/spaceservice/model/entity/Space.java
+++ src/main/java/com/eventspace/spring/spaceservice/model/entity/Space.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 @Entity
 @RequiredArgsConstructor
 @Data
+@Table(schema = "order_unit")
 public class Space {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
