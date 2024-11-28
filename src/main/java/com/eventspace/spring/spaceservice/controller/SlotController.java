1510383dbd8190bb2aa02c64fd5bf040cb9028e0
Qaraqat Almasova <92965747+Karlsooon@users.noreply.github.com>
2024-11-28 15:46:53 +0500

    Update SlotController.java

--- src/main/java/com/eventspace/spring/spaceservice/controller/SlotController.java
+++ src/main/java/com/eventspace/spring/spaceservice/controller/SlotController.java
@@ -14,6 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/slots")
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class SlotController {
 
     private final SlotService slotService;
