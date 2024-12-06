009fd01e42e560d431852000803f0e79636de644
Qaraqat Almasova <92965747+Karlsooon@users.noreply.github.com>
2024-12-06 08:54:14 +0500

    Update CommentRepository.java

--- src/main/java/com/eventspace/spring/spaceservice/repository/CommentRepository.java
+++ src/main/java/com/eventspace/spring/spaceservice/repository/CommentRepository.java
@@ -7,4 +7,6 @@ import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     List<Comment> findBySpaceId(Long spaceId);
+    List<Comment> findByUserEmail(String userEmail);
+
 }
