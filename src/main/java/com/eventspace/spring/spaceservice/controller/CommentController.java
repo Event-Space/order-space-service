26eba72ec91c94966600e6fd2f8fdd98c9a1cb48
Marat Mukhammed <mukhammedmarat04@gmail.com>
2024-12-06 07:25:33 +0500

    fix origin

--- src/main/java/com/eventspace/spring/spaceservice/controller/CommentController.java
+++ src/main/java/com/eventspace/spring/spaceservice/controller/CommentController.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/comments")
+@CrossOrigin("*")
 public class CommentController {
 
     private final CommentService commentService;
